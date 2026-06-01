fe0b8a302ed8133633012576522da77fef6b3eae
Jonathan Bowker <j.b.bowker@gmail.com>
2026-06-01 14:01:04 +0100

    Seed demo users in DigitalOcean runtime

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $users = [
+            [
+                'name' => 'Jonathan Bowker',
+                'email' => 'jonathan@advancedanalytica.co.uk',
+                'is_super_admin' => true,
+                'email_verified_at' => now(),
+            ],
+            [
+                'name' => 'Test Admin',
+                'email' => 'test@example.com',
+                'is_super_admin' => false,
+                'email_verified_at' => null,
+            ],
+        ];
+
+        foreach ($users as $attributes) {
+            User::updateOrCreate(
+                ['email' => $attributes['email']],
+                [
+                    'name' => $attributes['name'],
+                    'password' => Hash::make('password'),
+                    'is_super_admin' => $attributes['is_super_admin'],
+                    'email_verified_at' => $attributes['email_verified_at'],
+                    'disabled_at' => null,
+                ],
+            );
+        }
+    }
+}
